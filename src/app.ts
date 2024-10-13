6248251d3347c92354d444ee6c5d5d87e5f9eee0
BinyaminYacovson <bj57577@gmail.com>
2024-10-13 17:45:24 +0300

    added swagger

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,68 @@
-import express from "express";
-import dotenv from "dotenv";
-// import postRouter from "./routes/postRoutes";
-// import userRouter from "./routes/userRoutes";
-import { errorHandler } from "./middleware/errorHandler";
-import connectDB from "./config/db";
+import express from "express"
+import dotenv from "dotenv"
+import authRouter from "./routes/authRoutes";
+import teacherRouter from "./routes/teacherRoutes";
+import studentRouter from "./routes/studentRoutes"
+import { errorHandler } from "./middlewares/errorHandler"
+import connectDB from "./config/db"
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
 // Middleware
-app.use(express.json());
+app.use(express.json())
 
-connectDB();
+connectDB()
+
+// Swagger configuration
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Classroom API',
+      version: '1.0.0',
+      description: 'API for managing classroom activities',
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Routes
-// app.use("/api/posts", postRouter);
-// app.use("/api/users", userRouter);
-
+app.use("/api/students", studentRouter);
+app.use("/api/teachers", teacherRouter);
+app.use("/api/auth", authRouter);
 
 // Error handling middleware
-app.use(errorHandler);
+app.use(errorHandler)
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
 
-export default app;
+export default app
