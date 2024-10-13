12e72b635c9e7a5851088ae6ff7e8c83bfe9171c
BinyaminYacovson <bj57577@gmail.com>
2024-10-13 11:10:33 +0300

    app.ts and dbConfiguration basic stucture

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,30 @@
-// import express from "express";
-// import dotenv from "dotenv";
+import express from "express";
+import dotenv from "dotenv";
 // import postRouter from "./routes/postRoutes";
 // import userRouter from "./routes/userRoutes";
-// import { errorHandler } from "./middleware/errorHandler";
-// import connectDB from "./config/db";
+import { errorHandler } from "./middleware/errorHandler";
+import connectDB from "./config/db";
 
-// dotenv.config();
+dotenv.config();
 
-// const app = express();
-// const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// // Middleware
-// app.use(express.json());
+// Middleware
+app.use(express.json());
 
-// connectDB();
+connectDB();
 
-// // Routes
+// Routes
 // app.use("/api/posts", postRouter);
 // app.use("/api/users", userRouter);
 
 
-// // Error handling middleware
-// app.use(errorHandler);
+// Error handling middleware
+app.use(errorHandler);
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
-// export default app;
+export default app;
